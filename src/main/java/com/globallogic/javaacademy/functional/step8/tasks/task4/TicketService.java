4aea48ee915f965b45af4527597602ca05fc55a1
Patresss <piechaczek.patryk@gmail.com>
2022-07-26 09:43:32 +0200

    Add homework

--- src/main/java/com/globallogic/javaacademy/functional/step8/tasks/task4/TicketService.java
+++ src/main/java/com/globallogic/javaacademy/functional/step8/tasks/task4/TicketService.java
@@ -24,5 +24,29 @@ public class TicketService {
             throw new RuntimeException("Invalid ticket status");
         }
     }
+    public BigDecimal calculateFeeSwitch(final Ticket ticket) {
+        return switch (ticket.getTicketType()) {
+            case STANDARD -> BigDecimal.valueOf(1.00);
+            case PREMIUM -> ticket.getPrice().add(BigDecimal.valueOf(10.0));
+            case ULTIMATE -> {
+                final BigDecimal inflation = inflationProvider.provideInflation();
+                yield ticket.getPrice().multiply(inflation);
+            }
+        };
+    }
 
+    public BigDecimal calculateFeeEnum(final Ticket ticket){
+        return ticket.getTicketType().calculateFee( this, ticket);
+    }
+
+    public BigDecimal calculateStandardFee(final Ticket ticket){
+        return BigDecimal.valueOf(1.00);
+    }
+    public BigDecimal calculatePremiumFee(final Ticket ticket){
+        return ticket.getPrice().add(BigDecimal.valueOf(10.0));
+    }
+    public BigDecimal calculateUltimateFee(final Ticket ticket){
+        final BigDecimal inflation = inflationProvider.provideInflation();
+        return ticket.getPrice().multiply(inflation);
+    }
 }
--- src/main/java/com/globallogic/javaacademy/functional/step8/tasks/task4/TicketType.java
+++ src/main/java/com/globallogic/javaacademy/functional/step8/tasks/task4/TicketType.java
@@ -1,8 +1,22 @@
 package com.globallogic.javaacademy.functional.step8.tasks.task4;
 
+import java.math.BigDecimal;
+import java.util.function.BiFunction;
+
 public enum TicketType {
 
-    STANDARD,
-    PREMIUM,
-    ULTIMATE
+    STANDARD(TicketService::calculateStandardFee),
+    PREMIUM(TicketService::calculatePremiumFee),
+    ULTIMATE(TicketService::calculateUltimateFee);
+
+    private final BiFunction<TicketService, Ticket, BigDecimal> calculatePrice;
+
+    TicketType(final BiFunction<TicketService, Ticket, BigDecimal> calculatePrice) {
+        this.calculatePrice = calculatePrice;
+    }
+
+    public BigDecimal calculateFee(final TicketService ticketService, final Ticket ticket) {
+        return calculatePrice.apply(ticketService, ticket);
+    }
+
 }
